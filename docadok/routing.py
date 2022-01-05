cf1a364f67e3b9785f9f88e8aef11f872b0e4778
stephan ceroi <stephan.ceroi@gmail.com>
2022-01-05 17:51:47 +0100

    Revert "Revert "qsdfq""
    This reverts commit 8d129f510f1236e0ad902c15ee06203d45c1ea5f.

--- docadok/routing.py
+++ docadok/routing.py
@@ -1,12 +1,25 @@
 from django.urls import path
-from qcm.consumers import TableauConsumer
+from realtime.consumers import Consumer
 
 
-ws_urlpatterns = [
+"""ws_urlpatterns = [path('/RT/Cons/', Consumer.as_asgi())]
+"""
 
-	path('qcm/tableau/', TableauConsumer.as_asgi())
-	
-]
+
+from django.urls import path
+
+#from channels.http import AsgiHandler
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+application = ProtocolTypeRouter({
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            path("RT/Cons/", Consumer.as_asgi()),
+        ]),
+    ),
+
+})
 
 
 
